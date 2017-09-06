8f9916178ce1c323fea134c85beb5d4f6f2a9648
Steve Gilham <SteveGilham@users.noreply.github.com>
2017-09-06 22:12:54 +0100

    Include the ranges into the build

--- drophash/stdafx.h
+++ drophash/stdafx.h
@@ -9,9 +9,10 @@
 // #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #pragma warning (push, 1)
-#pragma warning (disable : 4355 4917 4702 26400 4987 4820 4365 4623 4625 4626 5026 5027 4571 4774 26412 26461)
+#pragma warning (disable : 4355 4917 4702 26400 4987 4820 4365 4623 4625 4626 5026 5027 4571 4774 26412 26461 26499)
 #include <gsl.h>
 #pragma warning (pop) // unbalanced push in span.h
+#include <range/v3/all.hpp>
 #include <windows.h>
 #include <Shlobj.h>
 #include <Strsafe.h>
