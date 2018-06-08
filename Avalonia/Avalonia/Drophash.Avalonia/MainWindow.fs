d4c92347f91969836de468339286f9bff96f8855
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-06-08 16:22:18 +0100

    Outline "About" tab

--- Avalonia/Avalonia/Drophash.Avalonia/MainWindow.fs
+++ Avalonia/Avalonia/Drophash.Avalonia/MainWindow.fs
@@ -1,11 +1,56 @@
 namespace Drophash.Avalonia
 
 open Avalonia.Controls
+open Avalonia.Controls.Html
 open Avalonia.Markup.Xaml
+
+open System
+open System.Globalization
+open System.Resources
+
+module UICommon =
+
+    let GetResourceString (key:string) =
+        let executingAssembly = System.Reflection.Assembly.GetExecutingAssembly()
+        let resources = new ResourceManager("Drophash.Avalonia.Strings", executingAssembly)
+        resources.GetString(key)
+
 
 type MainWindow () as this =
     inherit Window()
 
+    let mutable armed = false
+
     do this.InitializeComponent()
     member this.InitializeComponent() =
         AvaloniaXamlLoader.Load(this)
+        //this.Title <- UICommon.GetResourceString "About"
+        this.FindControl<TextBlock>("Program").Text <- "Drophash " + "version todo" //AssemblyVersionInformation.AssemblyFileVersion
+        this.FindControl<TextBlock>("Description").Text <- UICommon.GetResourceString "ProgramDescription"
+
+        let copyright = "copyright todo" //AssemblyVersionInformation.AssemblyCopyright
+        this.FindControl<TextBlock>("Copyright").Text <- copyright
+
+        let link = this.FindControl<HtmlLabel>("Link")
+        link.Text <- """<center><a href="http://www.github.com/SteveGilham">""" +
+                      UICommon.GetResourceString "WebsiteLabel" +
+                      "</a></center>"
+
+        link.PointerPressed |> Event.add (fun _ -> armed <- true)
+        link.PointerLeave |> Event.add (fun _ -> armed <- false)
+        link.PointerReleased
+        |> Event.add (fun _ -> ())
+
+        // Windows -- Process Start (url)
+        // Mac -- ("open", url)
+        // *nix -- ("xdg-open", url)
+        //Application.Instance.Open("http://www.github.com/SteveGilham"))
+
+        this.FindControl<TextBlock>("License").Text <- UICommon.GetResourceString "AboutDialog.License"
+        this.FindControl<TextBlock>("MIT").Text <- String.Format(CultureInfo.InvariantCulture,
+                                                                 UICommon.GetResourceString "License",
+                                                                 copyright)
+
+        let expander = this.FindControl<Expander>("Expander")
+        expander.PropertyChanged
+        |> Event.add (fun _ -> this.Height <- float (if expander.IsExpanded then 550 else 400))
