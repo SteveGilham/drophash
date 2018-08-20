a9bdceae15a20965edfe458ad5e613e65313032a
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-08-20 12:06:03 +0100

    No real improvement with the real range-v3 even if it clains VS201x compatibility

--- drophash/stdafx.h
+++ drophash/stdafx.h
@@ -9,7 +9,7 @@
 // #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #pragma warning (push, 1)
-#pragma warning (disable : 4244 4355 4917 4702 26400 4987 4820 4365 4623 4625 4626 5026 5027 4571 4774 26412 26461 26481 26493 26495 26496 26499 26444 26434)
+#pragma warning (disable : 4548 4244 4355 4917 4702 26400 4987 4820 4365 4623 4625 4626 5026 5027 4571 4774 26412 26461 26481 26493 26495 26496 26499 26444 26434)
 #include <gsl.h>
 #pragma warning (pop) // unbalanced push in span.h
 #include <range/v3/all.hpp>
